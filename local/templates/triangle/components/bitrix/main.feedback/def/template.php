12a0ec99a133ed9ff84bbc0c1892b8bafe774b6c
Valery Ulyanov <vauvalera@gmail.com>
2017-10-03 09:31:07 +0300

    Форма готова

--- local/templates/triangle/components/bitrix/main.feedback/def/template.php
+++ local/templates/triangle/components/bitrix/main.feedback/def/template.php
@@ -10,34 +10,33 @@ if(!defined("B_PROLOG_INCLUDED")||B_PROLOG_INCLUDED!==true)die();
  * @global CUser $USER
  */
 ?>
-<div class="mfeedback">
-<?if(!empty($arResult["ERROR_MESSAGE"]))
-{
-	foreach($arResult["ERROR_MESSAGE"] as $v)
-		ShowError($v);
-}
-if(strlen($arResult["OK_MESSAGE"]) > 0)
-{
-	?><div class="mf-ok-text"><?=$arResult["OK_MESSAGE"]?></div><?
-}
-?>
 
 <?//pre($arResult);?>
-<form id="main-contact-form" name="contact-form" method="post" action="<?=POST_FORM_ACTION_URI?>">
+<form action="<?=POST_FORM_ACTION_URI?>" method="POST">
+	<?if(!empty($arResult["ERROR_MESSAGE"]))
+	{
+		foreach($arResult["ERROR_MESSAGE"] as $v)
+			ShowError($v);
+	}
+	if(strlen($arResult["OK_MESSAGE"]) > 0)
+	{
+		?><div class="mf-ok-text"><?=$arResult["OK_MESSAGE"]?></div><?
+	}
+	?>
 	<?=bitrix_sessid_post()?>
 		<div class="form-group">
-			<input id="input-6" type="text" class="form-control" name="NAME" value="" placeholder="Name" required>
+			<input id="input-6" type="text" class="form-control" name="user_name" value="<?=$arResult["AUTHOR_NAME"]?>" placeholder="Валерий"<?if(empty($arParams["REQUIRED_FIELDS"]) || in_array("NAME", $arParams["REQUIRED_FIELDS"])):?> required<?endif?>>
 		</div>
 		<div class="form-group">
-			<input id="input-9" type="email" class="form-control" name="EMAIL" value="" placeholder="Email Id" required>
+			<input id="input-9" type="email" class="form-control" name="user_email" value="<?=$arResult["AUTHOR_EMAIL"]?>" placeholder="Email Id"
+						<?if(empty($arParams["REQUIRED_FIELDS"]) || in_array("EMAIL", $arParams["REQUIRED_FIELDS"])):?> required<?endif?>>
 		</div>
 		<div class="form-group">
-				<textarea class="form-control" rows="5" name="MESSAGE" id="input-10" placeholder="Your text here"></textarea>
+				<textarea class="form-control" rows="7" name="MESSAGE" id="input-10"<?if(empty($arParams["REQUIRED_FIELDS"]) || in_array("MESSAGE", $arParams["REQUIRED_FIELDS"])):?> required<?endif?>><?=$arResult["MESSAGE"]?></textarea>
 		</div>
 		<div class="form-group">
-				<input type="submit" name="submit" class="btn btn-submit" value="Submit">
+				<button class="btn btn-primary" type="submit">Отправить</button>
 		</div>
-
+			<input type="hidden" name="PARAMS_HASH" value="<?=$arResult["PARAMS_HASH"]?>">
+			<input type="hidden" name="submit" value="<?=GetMessage("MFT_SUBMIT")?>">
 </form>
-
-</div>
